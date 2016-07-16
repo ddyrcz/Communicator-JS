23321bcf753cd72818c37a284095e79be0a64e81
Dawid Dyrcz <dyrcu15@gmail.com>
2016-07-16 18:55:19 +0200

    Create simple communicator page, extract static file serve logic

--- server.js
+++ server.js
@@ -1,40 +1,14 @@
-var http = require('http'),
-	fs = require('fs'),
-	mime = require('mime'),
-	path = require('path');
+var http = require('http'),	
+	util = require('util'),
+	staticFile = require('./lib/static');
 
-var cache = {};
-	
-var serveStaticFile = function(filePath, res){
-	if(cache[filePath]){
-		console.log('load from cache');
-		sendFile(res, filePath, cache[filePath]);
-	}else{
-		fs.readFile(filePath, (err, data) => {
-			if(err){
-				notFound(res);
-			}else{
-				cache[filePath] = data;
-				console.log('load file');
-				sendFile(res, filePath, data);
-			}
-		});
-	}
-}
-	
-var notFound = function(res){
-	res.writeHead(404,{"content-type":"text/plain"});
-	res.end('404 not found');
-}
-
-var sendFile = function(res, filePath, data){
-	res.writeHead(200, {"content-type":mime.lookup(path.basename(filePath))});
-	res.end(data);	
-}
-	
 var server = http.createServer(function(req, res){
-	var filePath = 'public' + req.url;	
-	serveStaticFile(filePath, res);	
+	console.log(util.format('%s %s', req.method, req.url));	
+	
+	if(req.method === 'GET'){
+		var filePath = 'public' + req.url;	
+		staticFile.serveStaticFile(filePath, res);	
+	}
 });
 
 server.listen(1234);
