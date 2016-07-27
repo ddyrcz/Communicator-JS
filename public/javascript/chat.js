2b500f0c26dd34dd6fc6df3a2e416fa8aae0b2cc
Dawid <dyrcu15@gmail.com>
2016-07-27 13:04:32 +0200

    Prototype based object to class based

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -1,31 +1,32 @@
-var Chat = function (socket) {
-    this.socket = socket;
-}
+class Chat {
+    constructor(socket) {
+        this.socket = socket;
+    }
 
-Chat.prototype.initHandlers = function () {
+    manageUserInput() {
+        var message = $('#send-message').val()
+        $('#messages').append($('<div id=\'myMessage\'></div>').text('me: ' + message));
+        socket.emit('message', message);
+        $('#send-message').val('');
+    }
 
-    socket.on('message', (message) => {
-        $('#messages').append($('<div></div>').text(message));
-    });
+    initHandlers() {
+        socket.on('message', (message) => {
+            $('#messages').append($('<div></div>').text(message));
+        });
 
-    socket.on('name', (name) => {
+        socket.on('name', (name) => {
 
-    });
+        });
 
-    socket.on('room', (currentRoom) => {
-        $('#room').text(currentRoom);
-    });
+        socket.on('room', (currentRoom) => {
+            $('#room').text(currentRoom);
+        });
 
-    socket.on('rooms', (rooms) => {
-        $('#room-list').text(rooms);
-    });
+        socket.on('rooms', (rooms) => {
+            $('#room-list').text(rooms);
+        });
+    }
 }
 
 setInterval(() => { socket.emit('rooms') }, 1000);
-
-Chat.prototype.manageUserInput = function () {
-    var message = $('#send-message').val()
-    $('#messages').append($('<div id=\'myMessage\'></div>').text('me: ' + message));
-    socket.emit('message', message);
-    $('#send-message').val('');
-}
